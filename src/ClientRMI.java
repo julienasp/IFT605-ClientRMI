9a25684d4b493f0ba99b45fac0d68e51d4d4ee59
julienasp <julien.aspirot@usherbrooke.ca>
2016-09-28 20:07:55 -0400

    ajout security manager

--- src/ClientRMI.java
+++ src/ClientRMI.java
@@ -1,10 +1,8 @@
 import java.net.InetAddress;
 import java.rmi.Naming;
-import java.rmi.Remote;
-
 import core.BasicEquation;
-import core.MultiplicativeEquation;
-import core.SummativeEquation;
+//import core.MultiplicativeEquation;
+//import core.SummativeEquation;
 
 
 public class ClientRMI {
@@ -14,14 +12,17 @@ public class ClientRMI {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if (System.getSecurityManager() == null) {			
+			System.setSecurityManager(new SecurityManager());			
+		}
 		try {			
 			IRemoteEquation svr = (IRemoteEquation) Naming.lookup("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/ServeurRMI");		
 									
 				BasicEquation be = new BasicEquation(1,2);
-				SummativeEquation se = new SummativeEquation(be,be);
-				SummativeEquation se2 = new SummativeEquation(se,se);
+				//SummativeEquation se = new SummativeEquation(be,be);
+				//SummativeEquation se2 = new SummativeEquation(se,se);
 				
-				MultiplicativeEquation me = new MultiplicativeEquation(be,se);
+				//MultiplicativeEquation me = new MultiplicativeEquation(be,se);
 				
 				be.printUserReadable();
 				System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(be, 1)));
