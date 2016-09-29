0fefac3579544da1f0b1a3d7901238a365efaff6
julienasp <julien.aspirot@usherbrooke.ca>
2016-09-28 20:40:02 -0400

    fixs,mais stil lbugged

--- src/ClientRMI.java
+++ src/ClientRMI.java
@@ -1,5 +1,7 @@
 import java.net.InetAddress;
 import java.rmi.Naming;
+import java.rmi.Remote;
+
 import core.BasicEquation;
 //import core.MultiplicativeEquation;
 //import core.SummativeEquation;
@@ -16,8 +18,10 @@ public class ClientRMI {
 			System.setSecurityManager(new SecurityManager());			
 		}
 		try {			
-			IRemoteEquation svr = (IRemoteEquation) Naming.lookup("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/ServeurRMI");		
-									
+			//Remote r = (IRemoteEquation) Naming.lookup("rmi://" + InetAddress.getLocalHost().getHostAddress() + "/ServeurRMI");	
+			Remote r = (IRemoteEquation) Naming.lookup("rmi://localhost:1099/ServeurRMI");
+			System.out.println(r);			
+			if (r instanceof IRemoteEquation) {				
 				BasicEquation be = new BasicEquation(1,2);
 				//SummativeEquation se = new SummativeEquation(be,be);
 				//SummativeEquation se2 = new SummativeEquation(se,se);
@@ -25,9 +29,8 @@ public class ClientRMI {
 				//MultiplicativeEquation me = new MultiplicativeEquation(be,se);
 				
 				be.printUserReadable();
-				System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(be, 1)));
-				
-				
+				//System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(be, 1)));
+			}				
 		} catch (Exception e) {		
 			System.out.println(e.getMessage());	
 		
