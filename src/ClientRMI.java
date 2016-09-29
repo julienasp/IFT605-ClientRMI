8a3c01ef0a3c3bb163767b789bd145d32dba197a
julienasp <julien.aspirot@usherbrooke.ca>
2016-09-28 22:56:58 -0400

    fixed it

--- src/ClientRMI.java
+++ src/ClientRMI.java
@@ -1,10 +1,7 @@
-
 import java.rmi.Naming;
-import java.rmi.Remote;
-
 import core.BasicEquation;
-//import core.MultiplicativeEquation;
-//import core.SummativeEquation;
+import core.MultiplicativeEquation;
+import core.SummativeEquation;
 
 
 public class ClientRMI {
@@ -19,20 +16,30 @@ public class ClientRMI {
 		}
 		try {		
 				
-			Remote r = Naming.lookup("rmi://localhost:1099/ServeurRMI");
-			System.out.println(r);			
-			if (r instanceof IRemoteEquation) {				
-				BasicEquation be = new BasicEquation(1,2);
-				//SummativeEquation se = new SummativeEquation(be,be);
-				//SummativeEquation se2 = new SummativeEquation(se,se);
+			IRemoteEquation svr = (IRemoteEquation)Naming.lookup("rmi://localhost:1099/ServeurRMI");
+			System.out.println(svr);			
+			if (svr instanceof IRemoteEquation) {				
+				BasicEquation be = new BasicEquation(2,2);
+				SummativeEquation se = new SummativeEquation(be,be);
+				SummativeEquation se2 = new SummativeEquation(se,se);
 				
-				//MultiplicativeEquation me = new MultiplicativeEquation(be,se);
+				MultiplicativeEquation me = new MultiplicativeEquation(be,se);
 				
 				be.printUserReadable();
-				//System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(be, 1)));
+				System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(be, 1)));
+				
+				se.printUserReadable();
+				System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(se, 1)));
+				
+				se2.printUserReadable();
+				System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(se2, 1)));
+				
+				me.printUserReadable();
+				System.out.println("Client: La valeur pour l'équation ci-dessous avec un x=1 est: " + Double.toString(svr.getEquationValue(me, 1)));
 			}				
 		} catch (Exception e) {		
-			System.out.println(e.getMessage());	
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		
 		}
 	}
